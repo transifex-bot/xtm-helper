c361ebb8df735cf3465dac714b54e636888850c7
ehom <erwin.hom@puppet.com>
2019-02-10 14:50:44 -0800

    mention that base-url may need to be changed

--- scripts/xtm-list-project-files.bash
+++ scripts/xtm-list-project-files.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# If you are using a sandbox, you may need to change XTM_BASE_URL
+
 XTM_BASE_URL=https://www.xtm-cloud.com/project-manager-api-rest
 URL=$XTM_BASE_URL/projects
 
