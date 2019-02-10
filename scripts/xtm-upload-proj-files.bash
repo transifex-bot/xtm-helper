cc225b704c4800b44aa9d73410d11dde6cd9db43
ehom <erwin.hom@puppet.com>
2019-02-10 14:58:34 -0800

    init check-in of file upload script

---
+++ scripts/xtm-upload-proj-files.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# you may need to change the following URL 
+# if you are using a sandbox
+
+XTM_BASE_URL=https://www.xtm-cloud.com/project-manager-api-rest
+
+function uploadFiles() {
+  set -x
+
+  curl --silent \
+    --config xtm-basic-token.txt \
+    --form 'files[0].file=@./testfile1.txt' \
+    --form 'files[1].file=@./testfile2.txt' \
+    --form 'files[2].file=@./testfile3.txt' \
+    --form 'matchType=MATCH_NAMES' \
+    "$BASE_XTM_URL/projects/$1/files/upload" | json_pp
+}
+
+uploadFiles 1234
+
+echo
