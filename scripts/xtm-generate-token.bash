b89199fe84cf7a63b452100fe3fd238e41a23585
ehom <erwin.hom@puppet.com>
2019-02-10 21:53:44 -0800

    fill required field with sample values

--- scripts/xtm-generate-token.bash
+++ scripts/xtm-generate-token.bash
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
+# You may need to change the following URL if you are using a sandbox
+
 XTM_BASE_URL=https://www.xtm-cloud.com/project-manager-api-rest
 
-function getXtmToken {
-  curl --silent \
-    --data '{"client":"", "password":"", "userId": 0}' \
+curl --silent \
+    --data '{"client":"Acme", "password":"12345678", "userId": 88}' \
     --header "Content-Type: application/json" \
     $XTM_BASE_URL/auth/token | json_pp
-}
 
-getXtmToken
